7a5f647a261607d144293840805cf570ced95733
Robert Wiltshire <robert.wiltshire@futureplatforms.com>
2018-09-06 16:50:01 +0100

    Added Project

---
+++ MoveMatic5000/MoveMatic5000/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MoveMatic5000
+//
+//  Created by Robert Wiltshire on 06/09/2018.
+//  Copyright © 2018 Robert Wiltshire. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MoveMatic5000/MoveMatic5000/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MoveMatic5000
+//
+//  Created by Robert Wiltshire on 06/09/2018.
+//  Copyright © 2018 Robert Wiltshire. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
